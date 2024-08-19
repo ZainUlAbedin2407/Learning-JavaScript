0a3bea02b63591288ed0aea4c567af28e37bb4e0
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2024-08-19 16:55:58 +0500

    updating bookAssignment and slideAssignment file

---
+++ bookAssignment.js
@@ -0,0 +1,18 @@
+// Date :- 18-Aug-2024
+
+// 1 - Write a script to greet your website visitor using JS alert box
+
+alert("Hello User");
+
+
+// 2 - Write a script to display following message on your webpage 
+// Error! Please enter a valid Password
+
+alert("Error! Please enter a valid Password");
+
+
+// 3 - Write a script to display following message on your webpage: (Hint : Use line break)
+/*Welcome to JS land...
+  Happy Coding!*/
+
+alert("Welcome to JS land...\nHappy Coding!");
---
+++ slideAssignment.js
@@ -0,0 +1,37 @@
+// Date :- 17-Aug-2024
+
+// Assignment - 1
+
+// Create "firstName" and "lastName" variables
+let firstName;
+let lastName;
+
+// Assign values
+firstName = "Zain Ul Abedin";
+lastName = "M. Saleem";
+
+// Create "address" variable and assign "main street" value to it
+let address = "main street";
+address = "first street";
+
+// log all values in the console
+console.log(firstName);
+console.log(lastName);
+console.log(address);
+
+
+// Assignment - 2
+
+// Create "street" and "country" variables
+let street;
+let country;
+
+// Assign your values
+street = "Jodia Bazar, Karachi";
+country = "Pakistan";
+
+// Create "fullMailingAddress" variable and assign the result of "street + country" (remember about the space)
+fullMailingAddress = street + ", " + country;
+
+console.log(fullMailingAddress);
+
