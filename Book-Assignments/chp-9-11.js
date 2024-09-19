6f38eceba73d26770eca8b817b9e91eaa55b41ec
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2024-09-19 15:23:00 +0500

    completion of bookAssignments chp 9-11

--- Book-Assignments/chp-9-11.js
+++ Book-Assignments/chp-9-11.js
@@ -4,53 +4,57 @@
 
 let cityName = prompt("In which city do you live");
 
-if (cityName === "karachi"){
-    console.log("Welcome to city of lights");
+if (cityName === "karachi") {
+  console.log("Welcome to city of lights");
 }
 
 
 // 2 - Write a program to take "gender" as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma'am.
 
-let gender = prompt("What is your gender? (gender must be male/female and in lowercase)");
+let gender = prompt(
+  "What is your gender? (gender must be male/female and in lowercase)"
+);
 
-if (gender === "male"){
-    console.log("Good Morning Sir");
-} else if (gender === "female"){
-    console.log("Good Morning Ma'am");
+if (gender === "male") {
+  console.log("Good Morning Sir");
+} else if (gender === "female") {
+  console.log("Good Morning Ma'am");
 } else {
-    console.log("Invalid gender");
+  console.log("Invalid gender");
 }
 
 
 // 3 - Write a program to take input color of road traffic signal from the user & show the message according to this table:
 /*
-    Signal color     Message 
+    Signal color     Message
     Red              Must Stop
     Yellow           Ready to move
     Green            Move now
 */
 
-let trafficSignalColor = prompt("Enter the color of road traffic signal (red,green.yellow)");
+let trafficSignalColor = prompt(
+  "Enter the color of road traffic signal (red,green.yellow)"
+);
 
-if (trafficSignalColor === "red"){
-    console.log("Must Stop");
+if (trafficSignalColor === "red") {
+  console.log("Must Stop");
 } else if (trafficSignalColor === "yellow") {
-    console.log("Ready to move");
+  console.log("Ready to move");
 } else if (trafficSignalColor === "green") {
-    console.log( "Move now");
+  console.log("Move now");
 } else {
-    console.log("Invalid color");
+  console.log("Invalid color");
 }
 
 
 // 4 - Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message "Please refill the fuel in your car".
 
-let remainingFuel = +prompt("How much fuel availabe in your car? (in litres)")
+let remainingFuel = +prompt("How much fuel availabe in your car? (in litres)");
 
 if (remainingFuel < 0.25) {
-    console.log("Please refill the fuel in your car.");
+  console.log("Please refill the fuel in your car.");
 } else {
-    console.log("Enjoy Your Journey");
+  console.log("Enjoy Your Journey");
 }
 
 
@@ -78,7 +82,7 @@ if (remainingFuel < 0.25) {
     }
     if(c === 14){
     alert("condition 4 is true");
-        
+
     d. var materialCost = 20000;
     var laborCost = 2000;
     var totalCost = materialCost + laborCost;
@@ -93,61 +97,60 @@ if (remainingFuel < 0.25) {
     f. if("car"<"cat"){
     alert("car is smaller than cat");
     }
-*/ 
+*/
 
 // Part a
 let a = 4;
-if (++a ===5){
-    alert("given condition for variable a is true");
-}   // alert message displayed
+if (++a === 5) {
+  alert("given condition for variable a is true");
+} // alert message displayed
 
 // Part b
 let b = 82;
-if (b++===83){
-    alert("given condition for variable b is true");
-}   // alert message not displayed
+if (b++ === 83) {
+  alert("given condition for variable b is true");
+} // alert message not displayed
 
 // Part c
 let c = 12;
-if (c++ === 13){
-    alert("condition 1 is true");
-}   // alert message not displayed
+if (c++ === 13) {
+  alert("condition 1 is true");
+} // alert message not displayed
 
-if (c === 13){
-    alert("condition 2 is true");
-}   // alert message displayed 
+if (c === 13) {
+  alert("condition 2 is true");
+} // alert message displayed
 
-if (++c < 14){
-    alert("condition 3 is true");
-}   // alert message not displayed
+if (++c < 14) {
+  alert("condition 3 is true");
+} // alert message not displayed
 
-
-if(c === 14){
-    alert("condition 4 is true");
-}   // alert message displayed
+if (c === 14) {
+  alert("condition 4 is true");
+} // alert message displayed
 
 // Part d
 let materialCost = 20000;
 let laborCost = 2000;
 let totalCost = materialCost + laborCost;
 
-if (totalCost === laborCost + materialCost){
-    alert("The cost equals");
-}   // alert message displayed
+if (totalCost === laborCost + materialCost) {
+  alert("The cost equals");
+} // alert message displayed
 
 // Part e
-if (true){
-    alert("True");
-}   // alert message displayed
+if (true) {
+  alert("True");
+} // alert message displayed
 
-if (false) { 
-    alert("False");
-}   // alert message not displayed
+if (false) {
+  alert("False");
+} // alert message not displayed
 
 // Part f
-if("car"<"cat"){
-    alert("car is smaller than cat");
-}  // alert message displayed
+if ("car" < "cat") {
+  alert("car is smaller than cat");
+} // alert message displayed
 
 
 // 6 - Write a program to take input the marks obtained in three subjects & total marks. Compute & show the resulting percentage on your page. Take percentage & compute grade as per following table:
@@ -169,17 +172,17 @@ let grade;
 let remarks;
 
 if (percentage >= 80) {
-    grade = "A-one";
-    remarks = "Excellent";
+  grade = "A-one";
+  remarks = "Excellent";
 } else if (percentage >= 70) {
-    grade = "A";
-    remarks = "Good";
+  grade = "A";
+  remarks = "Good";
 } else if (percentage >= 60) {
-    grade = "B";
-    remarks = "You need to improve";
+  grade = "B";
+  remarks = "You need to improve";
 } else {
-    grade = "Fail";
-    remarks = "Sorry";
+  grade = "Fail";
+  remarks = "Sorry";
 }
 
 document.write("<h1>Marks Sheet</h1>");
@@ -188,3 +191,107 @@ document.write(`Marks Obtained : ${totalObtainedMarks}<br> `);
 document.write(`Percentage : ${percentage.toFixed(2)}%<br> `);
 document.write(`Grade : ${grade}<br> `);
 document.write(`Remarks : ${remarks}<br> `);
+
+
+// 7 - Guess game : Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number.
+/*
+    a. If user guesses the same number, show "Bingo! Correct answer".
+    b. If the guessed number +1 is the secret number, show "Close enough to the correct answer”.
+*/
+
+let secretNumber = 7;
+let guessNumber = +prompt("Guess the secret number between 1 to 10");
+
+if (guessNumber === secretNumber) {
+  alert("Bingo! Correct answer");
+} else if (guessNumber + 1 === secretNumber) {
+  alert("Close enough to the correct answer");
+} else {
+  alert("Try again");
+}
+
+
+// 8 - Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.
+
+let number = +prompt(
+  "Enter a number to check that it is divisible by 3 or not"
+);
+
+if (number % 3 === 0) {
+  alert("The number is divisible by 3");
+} else {
+  alert("The number is not divisible by 3");
+}
+
+
+// 9 -  Write a program that checks whether the given input is an even number or an odd number.
+
+let numbertoCheck = +prompt("Enter a number to check that it is even or odd.");
+
+if (numbertoCheck % 2 === 0 && numbertoCheck % 3 === 0) {
+  alert(`${numbertoCheck} is an even and odd number too.`);
+} else if (numbertoCheck % 2 === 0) {
+  alert(`${numbertoCheck} is an even number`);
+} else if (numbertoCheck % 3 === 0) {
+  alert(`${numbertoCheck} is an odd number`);
+} else {
+  alert("The number is not even nor odd");
+}
+
+
+// 10 - Write a program that takes temperature as input and shows a message based on following criteria
+/*
+    a. T > 40 then "It is too hot outside." 
+    b. T > 30 then "The Weather today is Normal.”
+    c. T > 20 then "Today's Weather is cool."
+    d. T > 10 then "OMG! Today's weather is so Cool."
+*/
+
+let temperature = +prompt("Enter a temperature");
+
+if (temperature >= 40) {
+    alert("It is too hot outside.");
+} else if (temperature >= 30) {
+    alert("The Weather today is normal.");
+} else if (temperature >= 20) {
+    alert("Today's Weather is cool.");
+} else {
+    alert("OMG! Today's weather is so Cool.");
+}
+
+
+// 11 - Write a program to create a calculator for +,-,*, / & % using if statements. Take the following input:
+/*
+    a. First number
+    b. Second number
+    c. Operation (+, -, *, /, %)
+    Compute & show the calculated result to user.
+*/
+
+let firstNumber = +prompt("Enter a first Number to perform Arithmetic operation");
+let secondNumber = +prompt("Enter a second Number to perform Arithmetic operation");
+let operation = prompt("Enter an operation (+, -, *, /, %) to perform");
+
+if (operation === "+") {
+    alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+} else if (operation === "-") {
+    alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+} else if (operation === "*") {
+    alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+} else if (operation === "/") {
+    if (secondNumber === 0) {
+        alert("Error! Division by zero is not allowed");
+    } else {
+        alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+    }
+} else if (operation ===  "%") {
+    if (secondNumber === 0) {
+        alert("Error! Division by zero is not allowed");
+    } else {
+        alert(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
+    }
+} else {
+    alert("Invalid Operator");
+}
+
+///////////////////////////////////////////////////////END////////////////////////////////////////////////////////////////
